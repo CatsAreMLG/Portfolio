9ca78ab7ce5663a8e25c96c544f054b6bea8587e
CatsAreMLG <joshuadgon@gmail.com>
2018-07-30 23:55:25 -0500

    Added Redcarpet & CodeRay for Markdown & Syntax Highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  class CodeRayify < Redcarpet::Render::HTML
+    def block_code(code, language)
+        CodeRay.scan(code, language).div
+    end
+  end
+  def markdown(content)
+      coderayified = CodeRayify.new(filter_html: true,  hard_wrap: true)
+      options = {
+        autolink: true,
+        no_intra_emphasis: true,
+        disable_indented_code_blocks: true,
+        fenced_code_blocks: true,
+        lax_html_blocks: true,
+        strikethrough: true,
+        superscript: true
+      }
+      markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
+      markdown_to_html.render(content).html_safe
+  end
 end
